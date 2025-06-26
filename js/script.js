258cb47294007a07df73760344007d6b939cd26a
Safira Carvalho <sasantosgo17@gmail.com>
2025-06-26 16:57:56 -0300

    adding accordion list

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function initAccordionList() {
+  const questions = document.querySelectorAll(".faq__question dt");
+  if (questions.length) {
+    questions[0].classList.add("active");
+    questions[0].nextElementSibling.classList.add("active");
+
+    questions.forEach((q) => q.addEventListener("click", handleClick));
+
+    function handleClick() {
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("active");
+    }
+  }
+}
+
+initAccordionList();
