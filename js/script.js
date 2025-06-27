4e187e2c119aa51133cefe77996bb1584a0f896f
Safira Carvalho <sasantosgo17@gmail.com>
2025-06-26 21:17:28 -0300

    menu hamburger

--- js/script.js
+++ js/script.js
@@ -29,6 +29,15 @@ function initHighlightLink() {
   }
 }
 
+const hamburger = document.querySelector(".header__hamburger");
+const nav = document.querySelector(".header__nav");
+
+hamburger.addEventListener("click", (e) => {
+  e.preventDefault();
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
 initHighlightLink();
 
 if (window.SimpleAnime) {
